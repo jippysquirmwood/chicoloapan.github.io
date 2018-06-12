6784f73197f6a4bac31769331fb987221fd9d86d
cyber2024 <russell.elfenbein@gmail.com>
2018-06-12 19:41:21 +0100

    added local mongo database, config etc. .env file is not included

--- Server/db.js
+++ Server/db.js
@@ -1,28 +1,46 @@
-let db = (function db(dbtype, db, config){
-    class DB{
-        constructor(config){
-            this.db = db;
-            this.db.connect(config.uri, config.db.options, function(err){
-                if(err) return console.log('Mongoose connection err.\n', err); 
-             });
-             this.connection =this.db.connection;
-             this.db.on('open', function(){
-               console.log('Database open.');
-               })
-               .on('error', function(e){
-               console.log('Database error.', e);
-               })
-               .on('close', function(){
-               console.log('Database closed.');
-               })
-               .on('disconnected', function(){
-                  console.log('Database disconnected.');
-                  setTimeout(function(){
-                      this.db.connect(config.uri, config.options);
-                  })
-               });
-        }
-    }
 
-    return {db: new db(config)};
-})(dbtype, uri, user, password);
+class Database{
+    constructor(db, uri, options){
+        this.db = db;     
+        this.connect(uri, options);
+    }
+    disconnect(){
+
+    }
+    connect(uri, options){
+
+        console.log('uri',uri)
+        this.db.connect(uri, options, 
+            function(err){
+                if(err) 
+                    return this.onError(err);
+                console.log('db connection successful');
+        });
+        
+        this.connection = this.db.connection;
+        this.connection
+            .on('open', (args)=>this.onConnectionOpen(args))
+            .on('error', (args)=>this.onError(args))
+            .on('close', (args)=>this.onConnectionClose(args))
+            .on('disconnected', (args)=>this.onDisconnect(args));
+            console.log('ere')
+    }
+    onConnectionOpen(){
+        console.log('Database open.');
+    }
+    onConnectionClose(){
+        console.log('Database closed.');
+    }
+    onError(error){
+        console.log('Database error.', error);
+    }
+    onDisconnect(){
+        console.log('Database disconnected.');
+        setTimeout(function(){
+            this.connect();
+        })
+    }
+}
+
+
+module.exports = Database;
--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,17 @@
-let express = require('express'); //an express server
-let app = express(); //initialize server
-let logger = require('morgan'); //server request logging
-let mongoose = require('mongoose'); //mongo connector
-let ejs = require('ejs'); //templating engine -> allows inserting js into html templates
-let bodyParser = require('body-parser');
-let path = require('path');
+const secrets = require('dotenv').config({path: process.cwd()+'/server/secrets/.env'}); //read out secret stuff not to be shared on github
+const express = require('express'); //an express server
+const app = express(); //initialize server
+const logger = require('morgan'); //server request logging
+const mongoose = require('mongoose'); //mongo connector
+const ejs = require('ejs'); //templating engine -> allows inserting js into html templates
+const bodyParser = require('body-parser');
+const path = require('path'); //concatenates paths using the correct encoding on different systems (unix, linux, win etc)
+const mongoConfig = require(path.join(process.cwd(),'server','config','mongo.config.js'));
+const Database = require(path.join(process.cwd(),'server','db.js'));
+let db = new Database(
+    mongoose,
+    mongoConfig.uri(process.env.DB_USER, process.env.DB_PWD, process.env.DB_HOST,process.env.DB_PORT, process.env.DB_NAME), 
+    mongoConfig.options);
 
 app.set('PORT', process.env.PORT || 8080); //http port 8080 usually, if run in some IDes they will put the requred port into process.env.PORT
 
@@ -17,7 +24,7 @@ app.use(logger('dev'));
 
 //convert parsed JSON strings into req.body
 app.use(bodyParser.json({
-    limit: '50mb'
+    limit: '50mb' // so we can send stuff to the server up to this limit.
 })); 
 
 
