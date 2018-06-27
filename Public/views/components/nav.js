b5b28cd30d8c832a85814365324765a6208428c7
David Ramírez Ramírez <david.ramirezr@mxsonda.com>
2018-06-27 12:45:44 -0500

    hre

--- Public/views/components/nav.js
+++ Public/views/components/nav.js
@@ -20,7 +20,7 @@ window.onclick = function(event) {
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+         openDropdown.classList.remove('show');
       }
     }
   }
