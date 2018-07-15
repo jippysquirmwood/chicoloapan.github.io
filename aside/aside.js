da17e656fa5c47fc77407f75b3dbb82e41380bba
jippysquirmwood <36202414+jippysquirmwood@users.noreply.github.com>
2018-07-15 13:40:51 +0100

    declaration

--- aside/aside.js
+++ aside/aside.js
@@ -1,10 +1,26 @@
+
+//declare all classes and elements in html doc here
+
+    //by element
+
+    //by class
+        let sidenav = document.getElementsByClassName("sidenav")
+        let closebtn = document.getElementsByClassName("closebtn")
+
+    //by id
+        let mySidenav = document.getElementById("mySidenav")
+        let main = document.getElementById("main")
+        let genericSpan = document.getElementById("genericSpan")
+
+
+
 let j=0
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     j=1;
-    document.getElementById("id span").innerText = "hello";
+    document.getElementById("id span").innerHTML = "hello";
 }
 
 function closeNav() {
