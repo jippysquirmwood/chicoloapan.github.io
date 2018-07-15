04696abaa52d5773a5ad7c42231fe4dfbdb4e6a1
jippysquirmwood <36202414+jippysquirmwood@users.noreply.github.com>
2018-07-15 13:42:33 +0100

    useDeclarations

--- aside/aside.js
+++ aside/aside.js
@@ -17,13 +17,13 @@
 let j=0
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
     j=1;
-    document.getElementById("id span").innerHTML = "hello";
+    mySidenav.style.width = "250px";
+    main.style.marginLeft = "250px";
+    genericSpan.innerHTML = "hello " + j;
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "30px";
-    document.getElementById("main").style.marginLeft= "30px";
+    mySidenav.style.width = "30px";
+    main.style.marginLeft= "30px";
 }
