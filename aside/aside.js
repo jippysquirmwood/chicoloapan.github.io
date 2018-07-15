3d614cc2c29205ed2e909c890858e0b787a877dd
jippysquirmwood <36202414+jippysquirmwood@users.noreply.github.com>
2018-07-15 13:25:11 +0100

    idvalue

--- aside/aside.js
+++ aside/aside.js
@@ -4,6 +4,6 @@ function openNav() {
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.width = "20";
     document.getElementById("main").style.marginLeft= "0";
 }
