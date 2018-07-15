20dfdc25abce5d16b5bfabda75cac31540a6d108
jippysquirmwood <36202414+jippysquirmwood@users.noreply.github.com>
2018-07-15 13:53:21 +0100

    ridConst

--- aside/aside.js
+++ aside/aside.js
@@ -1,33 +1,17 @@
-
-//declare all classes and elements in html doc here
-
-    //by element
-
-    //by class
-        const sidenav = document.getElementsByClassName("sidenav")
-        const closebtn = document.getElementsByClassName("closebtn")
-
-    //by id
-        const mySidenav = document.getElementById("mySidenav")
-        const main = document.getElementById("main")
-        const genericSpan = document.getElementById("genericSpan")
-
-
-
 var i=0;
 
 function openNav() {
     i++;
-    mySidenav.style.width = "250px";
-    main.style.marginLeft = "250px";
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
     genericSpan.innerHTML = "hello " + i;
 }
 
 function newFunction() {
-    j = 1;
+    i = 1;
 }
 
 function closeNav() {
-    mySidenav.style.width = "30px";
-    main.style.marginLeft= "30px";
+    document.getElementById("mySidenav").style.width = "30px";
+    document.getElementById("main").style.marginLeft = "30px";
 }
